84460fe0377bddfeffdae0372798a545acf44f74
MEI YIN LO <luomein@gmail.com>
2023-06-15 20:07:29 +0800

    Initial Commit

---
+++ Sources/SwiftUICytoscape/SwiftUICytoscape.swift
@@ -0,0 +1,6 @@
+public struct SwiftUICytoscape {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SwiftUICytoscapeTests/SwiftUICytoscapeTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftUICytoscape
+
+final class SwiftUICytoscapeTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftUICytoscape().text, "Hello, World!")
+    }
+}
